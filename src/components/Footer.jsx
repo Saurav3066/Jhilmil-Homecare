bd069cfdec80eefedbf250c3c666345e17f005da
Saurav Anand <contact.viktant3066@gmail.com>
2025-12-12 02:13:29 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,11 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="mt-auto w-full bg-gray-900 text-gray-300 py-6">
-      <div className="max-w-7xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center gap-4">
-        
+    <footer className="w-full bg-gray-900 text-gray-300 py-6 flex flex-col items-center">
         {/* Left Section */}
         <p className="text-sm">
           © {new Date().getFullYear()} Your Company. All rights reserved.
         </p>
-      </div>
     </footer>
   );
 }
